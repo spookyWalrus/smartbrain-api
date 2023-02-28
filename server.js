027674f077f177f055c53ae01ec8ed939ad05da3
Zen Nakamura <zenzenzen@gmail.com>
2023-02-28 07:51:31 -0500

    testing dotenv bundling

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.get('/',(req,res)=>{
 app.post('/signin',(req,res) => {signin.handleSignin(req,res,db,bcrypt) });
 
 app.post('/register',(req,res) => {
+	console.log('connected to the database: ', process.env.DATABASE, ' Clarifai is: ',process.env.CLARIFAI_AUTH_API_KEY);
 	register.handleRegister(req,res,db,bcrypt) });
 
 app.get('/profile/:id',(req,res) =>{profile.handleProfile(req,res,db) });
