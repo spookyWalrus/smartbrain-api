ef7257782b4ce2c0bf7c20d6d4552c58c52b52c5
Zen Nakamura <zenzenzen@gmail.com>
2023-05-15 11:19:02 -0400

    reverted error message

--- controllers/register.js
+++ controllers/register.js
@@ -29,7 +29,7 @@ const handleRegister = (req,res,db,bcrypt) => {
 		.then(trx.commit)
 		.catch(trx.rollback)
 	})
-	.catch(error => {res.status(400).json(error)})
+	.catch(error => {res.status(400).json('error with registration')})
 }
 
 module.exports = {
